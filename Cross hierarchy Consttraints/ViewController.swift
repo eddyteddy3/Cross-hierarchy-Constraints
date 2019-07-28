e2abf2cb530216bee531c17c76c37a2babf5b75e
Mouzzam <moazzamtahir81@gmail.com>
2019-07-28 23:29:17 +0500

    Outlets of constraints in order th remove the existing as well as outlets of Label and Button have been added

--- Cross hierarchy Consttraints/ViewController.swift
+++ Cross hierarchy Consttraints/ViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var label: UILabel!
+    @IBOutlet var button: UIButton!
+    @IBOutlet var viewB: UIView!
+    @IBOutlet var verticalConstraint: NSLayoutConstraint!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
